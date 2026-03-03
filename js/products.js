b220f3d6cb122016d37d37a3b0e3aa7d07577d49
Sofie Elkjær Larsen <sola0004@stud.ek.dk>
2026-03-03 12:31:12 +0100

    dynamisk produkter

--- js/products.js
+++ js/products.js
@@ -1,33 +1,42 @@
-const productContainer = document.querySelector(".product_list_container");
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
 
-fetch("https://kea-alt-del.dk/t7/api/products")
+fetch(`https://kea-alt-del.dk/t7/api/products/${id}`)
   .then((response) => response.json())
-  .then((data) => {
-    data.forEach((product) => {
-      // Tjek om produkt er udsolgt
-      let soldOutClass = product.soldout ? "udsolgt" : "";
+  .then((product) => {
+    let soldOutClass = product.soldout ? "udsolgt" : "";
 
-      // Tjek om produkt er nedsat
-      let discountHTML = "";
-      if (product.discount) {
-        const newPrice = Math.round(
-          product.price - (product.price * product.discount) / 100,
-        );
+    let discountHTML = "";
+    if (product.discount) {
+      const newPrice = Math.round(
+        product.price - (product.price * product.discount) / 100,
+      );
 
-        discountHTML = `
+      discountHTML = `
           <p class="old_price">${product.price} kr</p>
           <p class="discount">${newPrice} kr</p>
         `;
-      } else {
-        discountHTML = `<p class="price">${product.price} kr</p>`;
-      }
+    } else {
+      discountHTML = `<p class="price">${product.price} kr</p>`;
+    }
 
-      productContainer.innerHTML += `
-        <a href="produkt.html?id=${product.id}" class="product_card ${soldOutClass}">
-          <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="${product.productdisplayname}">
-          <h2>${product.productdisplayname}</h2>
-          ${discountHTML}
-        </a>
-      `;
-    });
+    productContainer.innerHTML = `  <div class="product_images">
+        <img src="https://kea-alt-del.dk/t7/images/webp/640/${id}.webp" alt="" />
+      </div>
+
+      <div class="product_info">
+        <h2>${product.productdisplayname}</h2>
+   
+     
+
+        <p><strong>Type:</strong>${product.articletype}</p>
+        <p><strong>Størrelse:</strong>${product.sizefitdesc}</p>
+        <p><strong>Sæson:</strong>${product.season}</p>
+     ${discountHTML}
+        ${soldOutClass}
+     
+        <button>Køb nu</button>
+      </div>`;
   });
+
+const productContainer = document.querySelector(".product_container");
