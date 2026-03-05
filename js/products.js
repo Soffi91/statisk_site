edafbc61383e9454799ece66319c126a6d1b2ab9
Sofie Elkjær Larsen <sola0004@stud.ek.dk>
2026-03-05 16:15:26 +0100

    done

--- js/products.js
+++ js/products.js
@@ -1,4 +1,5 @@
 const productContainer = document.querySelector(".product_container");
+
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 
@@ -8,36 +9,38 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${id}`)
     let soldOutClass = product.soldout ? "udsolgt" : "";
 
     let discountHTML = "";
+    let discountLabel = "";
+
     if (product.discount) {
       const newPrice = Math.ceil(
         product.price - (product.price * product.discount) / 100,
       );
 
       discountHTML = `
-          <p class="old_price">${product.price} kr</p>
-          <p class="discount">${newPrice} kr</p>
-        `;
+        <p class="old_price">${product.price} kr</p>
+        <p class="discount">${newPrice} kr</p>
+      `;
+
+      discountLabel = `<p class="procent">-${product.discount}%</p>`;
     } else {
       discountHTML = `<p class="price">${product.price} kr</p>`;
     }
 
-    productContainer.innerHTML = `  <div class="product_images">
-        <img src="https://kea-alt-del.dk/t7/images/webp/640/${id}.webp" alt="" />
+    productContainer.innerHTML = `
+      <div class="product_images">
+        <img src="https://kea-alt-del.dk/t7/images/webp/640/${id}.webp" alt="${product.productdisplayname}" />
       </div>
 
-      <div class="product_info">
+      <div class="product_info ${soldOutClass}">
         <h2>${product.productdisplayname}</h2>
-             <p class="procent">-${product.discount}%</p>
 
-     
+        ${discountLabel}
+         <p><strong>Brand:</strong> ${product.brandname}</p>
+        <p><strong>Type:</strong> ${product.articletype}</p>
+       
+        ${discountHTML}
 
-        <p><strong>Type:</strong>${product.articletype}</p>
-        <p><strong>Størrelse:</strong>${product.sizefitdesc}</p>
-        <p><strong>Sæson:</strong>${product.season}</p>
-     ${discountHTML}
-        ${soldOutClass}
-        
-     
         <button class="knap">Køb nu</button>
-      </div>`;
+      </div>
+    `;
   });
